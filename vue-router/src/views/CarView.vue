9081012dca1200e8e119421ddf450a766239db81
Imad Roshan <imad.roshan@gmail.com>
2023-07-17 16:20:53 +0200

    A Basic Cars app to learn vue-router

---
+++ vue-router/src/views/CarView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useRoute, RouterView, useRouter } from 'vue-router';
+import cars from "../data/cars.json"
+
+const route = useRoute()
+const router = useRouter()
+
+const carId = parseInt(route.params.id)
+const car = cars.find(c => c.id === carId)
+</script>
+
+<template>
+    <div v-if="car">
+        <h1>Car View</h1>
+        <p>{{ car.name }}</p>
+        <p>{{ car.year }}</p>
+        <p>{{ car.price }}</p>
+        <button @click="router.push(`/cars/${carId}/contact`)">Contact Info</button>
+        <RouterView />
+    </div>
+    <div v-else>
+        <h1>Car Not Found</h1>
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ vue-router/src/views/ContactView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useRoute } from "vue-router";
+import cars from "../data/cars.json"
+
+const route = useRoute()
+const car = cars.find(c => c.id === parseInt(route.params.id))
+</script>
+<template>
+    <h1> Contact</h1>
+    <p>{{car.contact.dealer}}</p>
+    <p>{{car.contact.number}}</p>
+</template>
