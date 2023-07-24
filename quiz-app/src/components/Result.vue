b087b849956ff97d64292562f4943b361a41b529
Imad Roshan <imad.roshan@gmail.com>
2023-07-24 17:10:28 +0200

    Core functionality is implemented

---
+++ quiz-app/src/components/Result.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const {quizQuestionLength, numberOfCorrectAnswers} = defineProps(['quizQuestionLength', 'numberOfCorrectAnswers'])
+</script>
+<template>
+    <div class="results">
+        <h1> Your Results </h1>
+        <h1>
+            {{numberOfCorrectAnswers}} / {{quizQuestionLength}}
+        </h1>
+        <RouterLink to="/">
+            <button>Go to Home</button>
+        </RouterLink>
+    </div>
+</template>
+
+<style scoped>
+.results{
+    text-align: center;
+    padding: 100px 0;
+}
+
+p{
+    font-size: 30px;
+    font-weight: bold;
+}
+h1{
+    font-size: 80px;
+}
+</style>
