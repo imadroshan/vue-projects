a27804798369ecadba2b224092c9328defd8d1f0
Imad Roshan <imad.roshan@gmail.com>
2023-07-25 00:39:05 +0200

    Basic single html element transition

--- animation/README.md
+++ animation/README.md
@@ -1,4 +1,4 @@
-# quiz-app
+# animation
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ animation/src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const showText = ref(true)
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <Transition name="fade">
+        <h1 v-if="showText">Hello World</h1>
+        <h1 v-else>Goodbye world</h1>
+      </Transition>
+    </div>
+    <button @click="showText=!showText">Toggle</button>
+  </main>
+  <RouterView />
+</template>
+
+<style scoped>
+.fade-enter-from{
+  opacity: 0;
+}
+.fade-enter-to{
+opacity: 1;
+}
+.fade-enter-active{
+  transition: all 3s ease;
+}
+.fade-leave-from{
+  opacity: 1;
+}
+.fade-leave-to{
+  opacity: 0;
+}
+.fade-leave-active{
+  transition: all 3s ease;
+}
+.container{
+  position: relative;
+}
+h1{
+  position: absolute;
+}
+button{
+  position: absolute;
+  margin-top: 60px;
+}
+</style>
