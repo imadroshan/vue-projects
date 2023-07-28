e4133d28c9829ce6c9dd119eccfe75d15d8890ae
Imad Roshan <imad.roshan@gmail.com>
2023-07-28 21:11:01 +0200

    ShowApp completed

---
+++ showsapp/src/components/Card.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const {image, name} = defineProps(['image', 'name'])
+</script>
+
+<template>
+    <NCard :title="name">
+        <template #cover>
+          <img :src="image">
+        </template>
+        <slot>
+
+        </slot>
+      </NCard>
+</template>
+
+
+<style scoped>
+/* Card Styles */
+
+.n-card {
+    max-width: 200px;
+    max-height: 300px;
+    margin: 10px 20px;
+
+}
+
+.n-card img {
+    height: 250px
+}
+
+p {
+    font-size: 10px;
+}
+
+.jobs {
+    display: flex;
+    flex-wrap: wrap;
+}
+</style>
