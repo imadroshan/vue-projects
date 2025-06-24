1ac74cc44794faaaf13b5c5daf85a8847c984637
Imad Roshan <imad.roshan@gmail.com>
2025-06-24 22:18:03 +0200

    Test change

--- vue-counter-app/src/App.vue
+++ vue-counter-app/src/App.vue
@@ -1,22 +1,21 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 const count = ref(0);
-
 </script>
 
 <template>
   <main>
     <div>
-    <h4> The current count is </h4>
-    <h1> {{ count }} </h1>
-    <button @click="count--"> - </button>
-    <button @click="count++"> + </button>
-  </div>
+      <h4>The current count iss</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
   </main>
 </template>
 
 <style scoped>
-main{
+main {
   background-color: aliceblue;
   width: 100vw;
   height: 100vh;
